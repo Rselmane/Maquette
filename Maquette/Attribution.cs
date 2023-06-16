7341dbd378bcf551facb08d4d29e209239c347ba
Liam Medkour <liam.medkour@etu.univ-smb.fr>
2023-06-16 14:59:32 +0200

    modif classe attrib tu coné

--- Maquette/Attribution.cs
+++ Maquette/Attribution.cs
@@ -14,30 +14,35 @@ namespace Maquette
         {
         }
 
-        public Attribution(int idMateriel, int idEnseignant, DateTime dateAttribution, string commentaire)
+        public Attribution(int idPersonnel, int idMateriel, DateTime dateAttribution)
         {
-            this.idMateriel = idMateriel;
-            this.idEnseignant = idEnseignant;
-            this.DateAttribution = dateAttribution;
-            this.Commentaire = commentaire;
+            IdPersonnel = idPersonnel;
+            IdMateriel = idMateriel;
+            DateAttribution = dateAttribution;
         }
 
-        public int idMateriel { get; set; }
-        public int idEnseignant { get; set; }
+        public Attribution(int idPersonnel, int idMateriel, DateTime dateAttribution, string commentaire) : this(idPersonnel, idMateriel, dateAttribution)
+        {
+            Commentaire = commentaire;
+        }
+
         public DateTime DateAttribution { get; set; }
         public string Commentaire { get; set; }
-
+        public int IdMateriel { get; set; }
+        public Materiel MaterielAttribution { get; set; }
+        public int IdPersonnel { get; set; }
+        public Personnel PersonnelAttribution { get; set; }
         public void Create()
         {
             DataAccess accesBD = new DataAccess();
-            String requete = $"Insert into EST_ATTRIBUE (IDPERSONNEL, IDMATERIEL, DATEATTRIBUTION, COMMENTAIREATTRIBUTION) VALUES({this.idEnseignant},{this.idMateriel},'{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}','{this.Commentaire}');";
+            String requete = $"Insert into EST_ATTRIBUE (IDPERSONNEL, IDMATERIEL, DATEATTRIBUTION, COMMENTAIREATTRIBUTION) VALUES({this.IdPersonnel},{this.IdMateriel},'{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}','{this.Commentaire}');";
             accesBD.SetData(requete);
         }
 
         public void Delete()
         {
             DataAccess accesBD = new DataAccess();
-            String requete = $"Delete from EST_ATTRIBUE where IDMATERIEL= {this.idMateriel} and IDPERSONNEL= {this.idEnseignant} and DATEATTRIBUTION='{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}';";
+            String requete = $"Delete from EST_ATTRIBUE where IDMATERIEL= {this.IdMateriel} and IDPERSONNEL= {this.IdPersonnel} and DATEATTRIBUTION='{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}';";
             accesBD.SetData(requete);
         }
 
@@ -51,7 +56,7 @@ namespace Maquette
             {
                 foreach (DataRow row in datas.Rows)
                 {
-                    Attribution a = new Attribution(int.Parse(row["IDMATERIEL"].ToString()), int.Parse(row["IDPERSONNEL"].ToString()),DateTime.Parse(row["DATEATTRIBUTION"].ToString()), (String)row["COMMENTAIREATTRIBUTION"]);
+                    Attribution a = new Attribution(int.Parse(row["IDPERSONNEL"].ToString()), int.Parse(row["IDMATERIEL"].ToString()), DateTime.Parse(row["DATEATTRIBUTION"].ToString()), (String)row["COMMENTAIREATTRIBUTION"]);
                     lesAttributs.Add(a);
                 }
             }
@@ -71,7 +76,7 @@ namespace Maquette
         public void Update()
         {
             DataAccess accesBD = new DataAccess();
-            String requete = $"UPDATE EST_ATTRIBUE SET DATEATTRIBUTION = '{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}', COMMENTAIREATTRIBUTION = '{this.Commentaire}' where IDMATERIEL= {this.idMateriel} and IDPERSONNEL= {this.idEnseignant} and DATEATTRIBUTION='{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}';";
+            String requete = $"UPDATE EST_ATTRIBUE SET DATEATTRIBUTION = '{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}', COMMENTAIREATTRIBUTION = '{this.Commentaire}' where IDMATERIEL= {this.IdMateriel} and IDPERSONNEL= {this.IdPersonnel} and DATEATTRIBUTION='{this.DateAttribution.Year}-{this.DateAttribution.Month}-{this.DateAttribution.Day}';";
             accesBD.SetData(requete);
         }
     }
