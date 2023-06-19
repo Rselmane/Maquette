b905c6759e854afee45bedfe90c6595160b6108a
Rayan Selmane <Rayan.Selmane@etu.univ-smb.fr>
2023-06-19 10:08:21 +0200

    IsEnabled pour TabSelection

--- Maquette/MainWindow.xaml.cs
+++ Maquette/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -52,13 +53,49 @@ namespace Maquette
             modifAtt.ContenuMateriel.Content = a.MaterielAttribution.Nom;
             modifAtt.tb_commentaire.Text = a.Commentaire;
             modifAtt.dt_atribution.Text = ((DateTime)a.DateAttribution).ToString();
+
+            int valeurvalid = 2;
+            bool[] tabIndex = new bool[4];
+            tabIndex[valeurvalid] = true;
+
+            for(int i=0; i<tabIndex.Length; i++)
+            {
+                TabItem tabItem = modifAtt.TabModif.Items[i] as TabItem;
+                tabItem.IsEnabled = tabIndex[i];
+            }
+
+
+
+
+
             modifAtt.ShowDialog();
         }
 
         private void ModifierMateriel(object sender, MouseButtonEventArgs e)
         {
             WindowModifier modifMat = new WindowModifier();
+            Materiel m = (Materiel)(MaterielList.SelectedItem);
             modifMat.TabModif.SelectedIndex = 0;
+            modifMat.cb_cate.SelectedIndex = m.CategorieMat.IdCategorie -1;
+            modifMat.tb_codebarre.Text = m.CodeBarre;
+            modifMat.tb_refConst.Text = m.ReferenceConstr;
+            modifMat.tb_nom.Text = m.Nom;
+            modifMat.Owner = this;
+
+            int valeurvalid = 0;
+            bool[] tabIndex = new bool[4];
+            tabIndex[valeurvalid] = true;
+
+            for (int i = 0; i < tabIndex.Length; i++)
+            {
+                TabItem tabItem = modifMat.TabModif.Items[i] as TabItem;
+                tabItem.IsEnabled = tabIndex[i];
+            }
+
+
+
+
+
             modifMat.ShowDialog();
         }
 
@@ -69,7 +106,18 @@ namespace Maquette
             Categorie a = (Categorie)(CateMateriel.SelectedItem);
             modifCat.Owner = this;
             modifCat.tbCateModif.Text = a.NomCategorie;
+            int valeurvalid = 1;
+            bool[] tabIndex = new bool[4];
+            tabIndex[valeurvalid] = true;
+
+            for (int i = 0; i < tabIndex.Length; i++)
+            {
+                TabItem tabItem = modifCat.TabModif.Items[i] as TabItem;
+                tabItem.IsEnabled = tabIndex[i];
+            }
+
             modifCat.ShowDialog();
+
         }
     }
 }
