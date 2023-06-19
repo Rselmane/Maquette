dbf4aa3d00aa86a0e61244780a5bdd70c4ad90f8
Rayan Selmane <Rayan.Selmane@etu.univ-smb.fr>
2023-06-19 18:04:10 +0200

    Projet test +modifer

---
+++ MaquetteTests/MaterielTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maquette;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquette.Tests
+{
+    [TestClass()]
+    public class MaterielTests
+    {
+        [TestMethod()]
+        public void MaterielTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MaterielTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MaterielTest2()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void FindAllTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void FindBySelectionTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+
+        }
+    }
+}
---
+++ MaquetteTests/PersonnelTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maquette;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquette.Tests
+{
+    [TestClass()]
+    public class PersonnelTests
+    {
+        [TestMethod()]
+        public void PersonnelTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PersonnelTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PersonnelTest2()
+        {
+
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void FindAllTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void FindBySelectionTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+
+        }
+    }
+}
